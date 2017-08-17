2f3933771d0d4de93cda8ceeb1043ed0418c0bde
supercz <supercaozhi@163.com>
2017-08-17 17:09:31 +0800

    1、项目初始化

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+// Route::get('home/index/index1', 'Home\IndexController@index');
+Route::get('home/index/index', 'Home\IndexController@index');
