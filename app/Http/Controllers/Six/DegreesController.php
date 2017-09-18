e64799f63a8415c804b9245f7aedffbac91dbd46
supercz <supercaozhi@163.com>
2017-09-18 23:59:40 +0800

    创建任意号段内的用户数

--- app/Http/Controllers/Six/DegreesController.php
+++ app/Http/Controllers/Six/DegreesController.php
@@ -4,22 +4,139 @@ namespace App\Http\Controllers\Six;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Redis;
 
 class DegreesController extends Controller
 {
-  // 生成171号段1亿个用户，每个用户有随机的150个联系人
+
   public function create_user()
   {
+    // 生成171号段1亿个用户，每个用户有随机的150个联系人
+    $start = 17100000000;
+    $end = 17100002000;
+    // $end = 17199999999;
     // 创建一个新用户
-
-    // 给新用户添加150名好友联系人
-
-
+    $friends_num = 10;
+    for ($i=$start; $i < $end; $i++) {
+      // 给新用户添加150名好友联系人
+      $friends = [];
+      for ($j=0; $j < $friends_num; $j++) {
+        // 保证新的好友不是自己
+        $friend = rand($start,$end);
+        while ($i == $friend) {
+          $friend = rand($start,$end);
+        }
+        $friends[] = $friend;
+        Redis::sadd("user:$friend",$i);
+      }
+      Redis::sadd("user:$i",$friends);
+    }
+    echo "OK";
   }
 
-  // 查找任意两个联系人之间的
+  // 查找任意两个联系人之间的距离
   public function separation()
   {
-
+    list($tmp1, $tmp2) = explode(' ', microtime());
+    $start_time = Redis::set('time',date('Y-m-d H:i:s',time()).substr($tmp1,1,6));
+    $start = 17100000000;
+    $end = 17100002000;
+    // $end = 17199999999;
+    // echo $user1 = rand($start,$end);
+    echo $user1 = 17100001512;
+    echo "</br>";
+    // echo $user2 = rand($start,$end);
+    echo $user2 = 17100000356;
+    // echo $user2 = 17100000819;
+    echo "</br>";
+    while ($user1 == $user2) {
+      echo $user2 = rand($start,$end);
+      echo "</br>";
+    }
+    // 判断user1与user2是否为一度好友
+    $result = Redis::sismember("user:$user1",$user2);
+    if ($result) {
+      echo "1度"."</br>";
+      echo $user1." -> ".$user2;
+    }else {
+      $friends = $this->find_friends($user1, $user2);
+      if (count($friends)) {
+        echo count($friends)."度</br>";
+        for ($j=0; $j < count($friends); $j++) {
+          echo $friends[$j]." -> ";
+        }
+        echo $user2;
+      }else {
+        echo "not found";
+      }
+      // // 获取user1所有好友
+      // $friend_user1 = Redis::smembers("user:$user1");
+      // for ($i=0; $i < count($friend_user1); $i++) {
+      //   $result = Redis::sismember("user:$friend_user1[$i]",$user2);
+      //   if ($result) {
+      //     echo "2"."</br>";
+      //     echo $user1." -> ".$friend_user1[$i]." -> ".$user2;
+      //     break;
+      //   }
+      // }
+    }
+    echo "</br>++++++++++++++</br>";
+    list($tmp1, $tmp2) = explode(' ', microtime());
+    $end_time = Redis::append('time',"</br>".date('Y-m-d H:i:s',time()).substr($tmp1,1,6));
+    echo Redis::get('time');
   }
+
+  public function find_friends($user1, $user2)
+  {
+    // echo $user1;
+    // echo "</br>";
+    // echo $user2;
+    // die;
+    static $depth = 2;
+    static $friend_relation = [];
+    echo "</br>----------$depth</br>";
+    $friend_relation[] = $user1;
+
+    // 获取user1所有好友
+    $friend_user1 = Redis::smembers("user:$user1");
+    for ($i=0; $i < count($friend_user1); $i++) {
+      // 判断$friend_user1[$i]与user2是否为好友
+      $result = Redis::sismember("user:".$friend_user1[$i],$user2);
+      if ($result) {
+        $friend_relation[] = $friend_user1[$i];
+        return $friend_relation;
+        // break;
+      }
+    }
+
+    if (count($friend_relation) == $depth-1) {
+      if (++$depth > 5) {
+        // echo "not found";
+        // echo $user1." -> ".$user2;
+        // $friend_relation[] = $user1;
+        return 0;
+      }else {
+        for ($k=0; $k < count($friend_user1); $k++) {
+          // $friend_relation[count($friend_relation)-1] = $friend_user1[$k];
+          $result2 = $this->find_friends($friend_user1[$k], $user2);
+          if ($result2) {
+            // return 1;
+            return $result2;
+            break;
+          }
+        }
+        return 0;
+      }
+    }else {
+      # code...
+    }
+
+
+      // for ($i=0; $i < count($friend_user1); $i++) {
+      //   $this->find_friends($friend_user1[$i],$user2);
+      // }
+  }
+
+  // 每新增一个新用户建立新的六度分隔关系
+
 }
