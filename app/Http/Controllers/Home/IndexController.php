430af5dca66c6526d7ff3c6a04550fcb078a612c
supercz <supercaozhi@163.com>
2017-09-30 12:34:39 +0800

    no message

--- app/Http/Controllers/Home/IndexController.php
+++ app/Http/Controllers/Home/IndexController.php
@@ -8,5 +8,10 @@ use App\Http\Controllers\Controller;
 class IndexController extends Controller{
   public function index(){
     
+    list($tmp1, $tmp2) = explode(' ', microtime());
+    echo $start_time = date('Y-m-d H:i:s',time()).substr($tmp1,1,7);
+    $start_time = time().substr($tmp1,1,7);
+    echo "</br>";
+    echo "</br>";
   }
 }
