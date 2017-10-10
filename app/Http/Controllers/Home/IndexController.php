87b37aef2f422f1f9bd2ea010b3f6b58ca68d64e
supercz <supercaozhi@163.com>
2017-10-10 09:19:29 +0800

    删除无用注释

--- app/Http/Controllers/Home/IndexController.php
+++ app/Http/Controllers/Home/IndexController.php
@@ -55,14 +55,5 @@ class IndexController extends Controller{
     echo "<br>";
     echo json_encode(usort($array,$this->my_sort()));
 
-    // pos() 函数是 current() 函数 的别名。它可返回数组中当前元素的值。
-    // next() 函数把指向当前元素的指针移动到下一个元素的位置，并返回当前元素的值。
-    // natsort() 函数用自然顺序算法对给定数组中的元素排序。
-    // natcasesort() 函数用不区分大小写的自然顺序算法对给定数组中的元素排序。
-    // ksort() 函数按照键名对数组排序，为数组值保留原来的键。
-    // krsort() 函数将数组按照键逆向排序，为数组值保留原来的键。
-    // key() 函数返回数组内部指针当前指向元素的键名。
-    // in_array() 函数在数组中搜索给定的值。
-    // extract() extract() 函数
   }
 }
