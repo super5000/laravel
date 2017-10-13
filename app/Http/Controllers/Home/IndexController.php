fdc9b98c40a4951a3f1c929de7e94cdd83069b86
supercz <supercaozhi@163.com>
2017-10-13 10:34:38 +0800

    no message

--- app/Http/Controllers/Home/IndexController.php
+++ app/Http/Controllers/Home/IndexController.php
@@ -16,26 +16,6 @@ class IndexController extends Controller{
     echo "</br>";
     echo "</br>";
 
-    // $hjk = get_browser(null,true);
-    // echo json_encode($hjk);
-    // eval($hjk);
-    // for ($i=0; $i < 100; $i++) {
-    //   echo uniqid()."</br>";
-    // }
-
-    // $filename = base_path()."/test.log";
-    // echo json_encode(unpack("C*",md5_file($filename,TRUE)));
-    // echo "</br>";
-    // echo var_dump(md5(file_get_contents($filename)));
-    // echo "</br>";
-    // // echo var_dump(md5("/Users/caozhi/SITE/laravel/test.log/Users/caozhi/SITE/laravel/test.log/Users/caozhi/SITE/laravel/test.log/Users/caozhi/SITE/laravel/test.log/Users/caozhi/SITE/laravel/test.log/Users/caozhi/SITE/laravel/test.log"));
-    // echo "</br>";
-    // $str = "bcd+efg-hi*jk,lnabopqrs,t<br>uvwa.ba\bcxy";
-    // var_dump($str);
-    // echo "</br>";
-    // var_dump(htmlentities(html_entity_decode($str)));
-    // $str = "alkdhfgkjasdfhjsdgkj687678ykjasdhkjfdfhjjh    \n
-    // ";
     // echo "</br>";
     // echo var_dump(($str));
     // echo "</br>";
